a8f7a4e62b0026054ba1c3b1cb9679f538e2c947
Raghav Raj Mittal <raj.raghav@gmail.com>
2018-03-03 03:06:38 -0500

    servo stuff

--- servo/servo.ino
+++ servo/servo.ino
@@ -5,36 +5,34 @@
 Servo myservo;  // create servo object to control a servo
 
 int pos = 0;    // variable to store the servo position
-int tim = 0;
 
 void setup() {
   myservo.attach(9);  // attaches the servo on pin 9 to the servo object
   Serial.begin(9600);
+  pos = myservo.read();
 }
 
 void loop() {
-
-  pos = myservo.read();
   
   if (Serial.available()) {
         String str = Serial.readString();
         char dir = str[0];
-        int tim_str = (str[1]-'0')*10 + (str[2]-'0');
+        int tim = (str[1]-'0')*10 + (str[2]-'0');
         Serial.println(dir);
         Serial.println(tim_str);
 
         if (dir == 'R') {
-          for (int i = 0; i <= tim_str ; i += 1) {
+          for (int i = 0; i <= tim ; i += 1) {
             pos = pos + 1;
             myservo.write(pos);
-            delay(100);
+            delay(10);
           }
        
         } else if (dir == 'L') {
-          for (int i = 0; i <= tim_str ; i += 1) {
+          for (int i = 0; i <= tim ; i += 1) {
             pos = pos - 1;
             myservo.write(pos);
-            delay(100);
+            delay(10);
           }
         }
         
